48534aa041cb50c1ecf409130d11c32e1212a130
unknown <ashnoorchhabra1313@gmail.com>
2026-05-28 01:32:03 +0530

    Added backend root route

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,10 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Alertify Backend Running Successfully");
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/community', communityRoutes);
 
